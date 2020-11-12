3c2413c2e6b7d58fbc387240ca47655c513d803d
Andrzej Nowik <andrzej@mat.ug.edu.pl>
2020-11-12 10:40:11 +0100

    TMenu

---
+++ TMenu.h
@@ -0,0 +1,13 @@
+#pragma once
+#include "define.h"
+class TMenu {
+private:
+	vector<string> tabItemsV;
+public:
+	void add(string item);
+	void addAll(int ilePar, ...);
+	unsigned int wybierz();
+	TMenu();
+	~TMenu();
+};
+
